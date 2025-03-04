7d4dd43cf2fa8b421973fefc82e56280d9e615c8
neha-infi <neha@infiwebsolutions.com>
2025-03-04 15:43:49 +0530

    search testing

---
+++ src/wwSection.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="PDF-viewer" id="app">
+    <h1 :style="textStyle">My Title</h1>
+    <p>PDF URL: {{ this.content.pdf }}</p>
+    <vue-pdf-app v-if="content.pdf" style="height: 100vh;" :pdf="this.content.pdf"></vue-pdf-app>
+    <p v-else>No PDF available</p>
+  </div>
+</template>
+
+<script>
+import VuePdfApp from "vue3-pdf-app";
+import "vue3-pdf-app/dist/icons/main.css";
+
+export default {
+  components: {
+    VuePdfApp
+  },
+  props: {
+    content: {
+      type: Object,
+      default: () => ({ pdf: this.content.pdf })
+    }
+  },
+  data() {
+    return {
+      idConfig: { findInput: "member" }
+    };
+  },
+  methods: {
+    initObserver() {
+      this.resizeObserver = new ResizeObserver(() => {
+        console.log(this.content.pdf);
+        console.log("Resized");
+      });
+      this.resizeObserver.observe(this.$el);
+    },
+    clearObserver() {
+      if (this.resizeObserver) {
+        this.resizeObserver.disconnect();
+      }
+    }
+  },
+  beforeUnmount() {
+    this.clearObserver();
+  },
+  mounted() {
+    this.initObserver();
+    console.log("PDF URL:", this.content.pdf); // Debugging output
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.PDF-viewer{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
+</style>
---
+++ ww-config.js
@@ -0,0 +1,20 @@
+export default {
+  editor: {
+    label: {
+      en: "PDF viewer",
+    },
+    icon: 'document-report',
+  },
+  properties: {
+    pdf: {
+      label: { en: "PDF Url", fr: "PDF Url" },
+      type: "Text",
+      options: {
+        placeholder: "PDF url",
+      },
+      defaultValue:
+        "https://bsiybqzphvbftfzexyhc.supabase.co/storage/v1/object/public/bill_documents/docs/59312_6076.pdf",
+      bindable: true,
+    },
+  },
+};
